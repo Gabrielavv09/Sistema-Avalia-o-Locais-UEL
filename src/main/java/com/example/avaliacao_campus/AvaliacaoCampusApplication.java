076f2ba03e2810e692665125efd1fdece94154dd
gabriela <gabriela.vieira0@uel.br>
2025-10-23 11:44:05 -0300

    feat: Configuração inicial do projeto Spring Boot e dependências

---
+++ src/main/java/com/example/avaliacao_campus/AvaliacaoCampusApplication.java
@@ -0,0 +1,13 @@
+package com.example.avaliacao_campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoCampusApplication.class, args);
+	}
+
+}
